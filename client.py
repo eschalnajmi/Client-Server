0e186c7fad3f3231f87b44e79504081c9225b186
eschalnajmi <eschal.najmi@gmail.com>
2024-10-15 13:41:34 +0100

    initial commit

---
+++ client.py
@@ -0,0 +1,60 @@
+import os
+import socket
+
+def getdir():
+    while True:
+        source = input("Enter the source directory path: ")
+
+        if os.path.exists(source):
+            return source
+        
+        print("Invalid path. Please try again.")
+
+def sendfiles(newfiles, source):
+    successfullyadded = []
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('0.0.0.0',8080))
+
+    for f in newfiles:
+        client.send(f"{f}".encode())
+        print(f"Sent file {f}")
+
+        server_msg = client.recv(4096).decode()
+        if server_msg != "Success":
+            print(f"Error with file {f}")
+            return []
+        
+        contents = open(os.path.join(source, f),"r").read()
+        
+        client.send(f"{contents}".encode())
+        print(f"Sent contents of {contents}\n\n")
+        server_msg = client.recv(4096).decode()
+        if server_msg != "Success":
+            print(f"Error with file {f}")
+            return []
+        successfullyadded.append(f)
+
+    client.close()
+    return successfullyadded
+
+def getfiles(source):
+    addedfiles=[]
+
+    while True:
+        allfilenames = os.listdir(source)
+        newfiles = [f for f in allfilenames if f not in addedfiles]
+
+        if len(newfiles)==0:
+            continue
+
+        try:
+            addedfiles+=sendfiles(newfiles, source)
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return
+
+
+if __name__ == "__main__":
+    source = getdir()
+    getfiles(source)
---
+++ server.py
@@ -0,0 +1,46 @@
+import os
+import socket
+
+def getdir():
+    addedfiles = []
+    destination = input("Enter the destination directory path: ")
+
+    if not(os.path.exists(destination)):
+        os.mkdir(destination)
+        print(f"Created directory {destination}\n\n")
+    else:
+        addedfiles += os.listdir(destination) 
+            
+    return destination, addedfiles
+
+def connect(destination,addedfiles):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0',8080))
+    server.listen(5)
+
+    while True:
+        client, addr = server.accept()
+        print(f"Connection from {addr}")
+
+        while True:
+            filename = client.recv(4096).decode()
+            if filename == "": break
+            file = open(os.path.join(destination, filename),"w")
+            print(f"Created file {filename}")
+            client.send("Success".encode())
+
+            contents = client.recv(4096).decode()
+            if contents == "": 
+                print("no contents")
+                file.close()
+            print(f"Written contents {filename}")
+            client.send("Success".encode())
+
+            print(f"Received file {filename}\n\n")
+            addedfiles.append(filename)
+
+        client.close()
+        
+if __name__ == "__main__":
+    destination, addedfiles = getdir()
+    connect(destination, addedfiles)
