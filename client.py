85cb2d1866c1a2f4d5c89438682e7bd692416353
eschalnajmi <eschal.najmi@gmail.com>
2024-10-15 15:01:51 +0100

    feat: docstrings

--- client.py
+++ client.py
@@ -2,6 +2,10 @@ import os
 import socket
 
 def getdir():
+    '''
+    Gets the source directory path from the user, if invalid path is entered, prompts the user to enter again.
+    :return: source directory path
+    '''
     while True:
         source = input("Enter the source directory path: ")
 
@@ -11,13 +15,18 @@ def getdir():
         print("Invalid path. Please try again.")
 
 def sendfiles(newfiles, source):
+    '''
+    Sends all new files to the server.
+    :param newfiles: list of new files
+    :param source: source directory path
+    :return: list of successfully added files
+    '''
     successfullyadded = []
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('0.0.0.0',8080))
 
     for f in newfiles:
         client.send(f"{f}".encode())
-        print(f"Sent file {f}")
 
         server_msg = client.recv(4096).decode()
         if server_msg != "Success":
@@ -29,7 +38,6 @@ def sendfiles(newfiles, source):
         if contents == "":
             contents = f"\0"
         client.send(f"{contents}".encode())
-        print(f"Sent contents of {contents}\n\n")
         server_msg = client.recv(4096).decode()
         if server_msg != "Success":
             print(f"Error with file {f}")
@@ -40,6 +48,10 @@ def sendfiles(newfiles, source):
     return successfullyadded
 
 def getfiles(source):
+    '''
+    Gets all files from the source directory and sends any new ones to the server.
+    :param source: source directory path
+    '''
     addedfiles=[]
 
     while True:
