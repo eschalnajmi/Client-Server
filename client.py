eed287cc3ff947a2204b4f5f82204835eab7a03c
eschalnajmi <eschal.najmi@gmail.com>
2024-10-21 12:43:14 +0100

    feat: cli input - search broken rn

--- client.py
+++ client.py
@@ -1,40 +1,58 @@
 import os
+import glob
 import socket
+import hashlib
+import sys
+import pathlib
 
 def getdir():
-    '''
+    """
     Gets the source directory path from the user, if invalid path is entered, prompts the user to enter again.
     :return: source directory path
-    '''
-    while True:
-        source = input("Enter the source directory path: ")
+    """
+    if len(sys.argv) < 2:
+        return os.getcwd()
+    
+    return os.path.join(os.getcwd(),sys.argv[1])
 
-        if os.path.exists(source):
-            return source
-        
-        print("Invalid path. Please try again.")
+def recursiveget(source, f):
+    """
+    Recursively gets directories.
+    :param source: source directory path
+    :param f: file or directory name
+    :return: full path of the file or directory
+    """
+    allfiles = []
+    fullpath = os.path.join(source, f)
+    print(f"FULL PATH: {fullpath}")
+    for file in os.listdir(fullpath):
+        if os.path.isdir(file):
+            allfiles+=recursiveget(fullpath, file)
+        else:
+            allfiles.append(os.path.join(fullpath,file))
+
+    return allfiles
 
 def sendfiles(newfiles, source):
-    '''
+    """
     Sends all new files to the server.
     :param newfiles: list of new files
     :param source: source directory path
-    :return: list of successfully added files
-    '''
+    :return: list of successfully added files and their contents
+    """
     successfullyadded = []
+    newcontents = [] # stores hash of new contents
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('0.0.0.0',8080))
 
     for f in newfiles:
         client.send(f"{f}".encode())
-
         server_msg = client.recv(4096).decode()
         if server_msg != "Success":
             print(f"Error with file {f}")
             return []
         
         contents = open(os.path.join(source, f),"r").read()
-
         if contents == "":
             contents = f"\0"
         client.send(f"{contents}".encode())
@@ -42,27 +60,52 @@ def sendfiles(newfiles, source):
         if server_msg != "Success":
             print(f"Error with file {f}")
             return []
+        
         successfullyadded.append(f)
+        newcontents.append(hashlib.md5(contents.encode()).hexdigest())
+        print(f"Sent file {f}\n\n")
 
     client.close()
-    return successfullyadded
+    return successfullyadded,newcontents
 
 def getfiles(source):
-    '''
+    """
     Gets all files from the source directory and sends any new ones to the server.
     :param source: source directory path
-    '''
+    """
     addedfiles=[]
+    addedcontents=[] # stores hash of added contents
 
+    for f in list(glob.glob(os.path.join(source, "*"))):
+        addedcontents.append(hashlib.md5(open(os.path.join(source, f),"r").read().encode()).hexdigest())
+
+    count = 0
     while True:
-        allfilenames = os.listdir(source)
+        for f in list(glob.glob(os.path.join(source, "*"))):
+            if os.path.isfile(f):
+                allfilenames = os.listdir(source)
+        
         newfiles = [f for f in allfilenames if f not in addedfiles]
 
+        if count !=0 and count % 5 == 0:
+            for i, f in enumerate(addedfiles):
+                if hashlib.md5(open(os.path.join(source, f),"r").read().encode()).hexdigest() != addedcontents[i]:
+                    print(f"File {f} has been modified.")
+                    newfiles.append(f)
+                    addedcontents.pop(i)
+                    addedfiles.pop(i)
+
+
         if len(newfiles)==0:
+            count+=1
             continue
 
         try:
-            addedfiles+=sendfiles(newfiles, source)
+            newlyaddedfiles,newcontents=sendfiles(newfiles, source)
+            addedfiles+=newlyaddedfiles
+            print(f"added the files {newlyaddedfiles}")
+            addedcontents+=newcontents
+            count+=1
             
         except Exception as e:
             print(f"Error: {e}")
@@ -71,4 +114,15 @@ def getfiles(source):
 
 if __name__ == "__main__":
     source = getdir()
-    getfiles(source)
+
+    print(f"SOURCE: {source}")
+
+    for f in os.listdir(source):
+        print("F: ",f)
+        if(os.path.isfile(f)):
+            print("file " + f)
+
+        if os.path.isdir(f):
+            print("directory " + recursiveget(source, f))
+    
+    #getfiles(source)
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import os
 import socket
+import sys
 
 def getdir():
-    '''
+    """
     Gets the destination directory path from the user.
     :return: destination directory path and list of files already in the directory
-    '''
+    """
     addedfiles = []
-    destination = input("Enter the destination directory path: ")
 
+    if len(sys.argv) < 2:
+        destination = "destination"
+    else:
+        destination = sys.argv[1]
+   
     if not(os.path.exists(destination)):
         os.mkdir(destination)
         print(f"Created directory {destination}\n\n")
@@ -17,12 +22,21 @@ def getdir():
             
     return destination, addedfiles
 
+def createfullpath(destination, filename):
+    """
+    Creates the full path of the file.
+    :param destination: destination directory path
+    :param filename: name of the file
+    :return: full path of the file
+    """
+    return os.path.join(destination, filename)
+
 def connect(destination,addedfiles):
-    '''
+    """
     Connects to the client and receives files from the client.
     :param destination: destination directory path
     :param addedfiles: list of files already in the directory
-    '''
+    """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0',8080))
     server.listen(5)
@@ -34,6 +48,10 @@ def connect(destination,addedfiles):
         while True:
             filename = client.recv(4096).decode()
             if filename == "": break
+
+            if not os.path.exists(os.path.join(destination, os.path.dirname(filename))):
+                os.makedirs(os.path.join(destination, os.path.dirname(filename)))
+
             file = open(os.path.join(destination, filename),"w")
             client.send("Success".encode())
 
